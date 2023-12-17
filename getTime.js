c0c084071926a677c885c745a7b962ecc624d6a5
tsiviv <t0527698098@gmail.com>
2023-12-17 13:47:44 +0200

    Initial commit

--- getTime.js
+++ getTime.js
@@ -2,7 +2,7 @@ async function getTimeZoneInfo(latitude, longitude) {
     const apiKey = 'YOUR_GOOGLE_MAPS_API_KEY'; // הכנס/י את המפתח שלך כאן
   
     const apiUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${latitude},${longitude}&timestamp=${Math.floor(Date.now() / 1000)}&key=${apiKey}`;
-  
+  console.log("sljfj")
     try {
       const response = await fetch(apiUrl);
       const data = await response.json();
